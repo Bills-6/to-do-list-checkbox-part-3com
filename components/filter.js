3875e33fa32a2824d60e921dd68a8362afd11fa3
Bills-6 <conutconut6@gmail.com>
2025-08-28 22:51:53 +0700

    validasi target luar

--- components/filter.js
+++ components/filter.js
@@ -25,3 +25,21 @@ function isPanelVisibility(isForVisibility) {
 		}, 130);
 	}
 }
+
+// hide panel via button
+const __hideButtonPanelFilter = document.querySelector(".close-panel-button");
+__hideButtonPanelFilter.addEventListener("click", () => isPanelVisibility("closed"));
+
+// hide panel via Escape button
+document.addEventListener("keydown", function(isKey) {
+	if (isKey.key === "Escape") {
+		isPanelVisibility("closed");
+	}
+});
+
+// hidden panel if click is not the target
+document.addEventListener("click", (e) => {
+	if (!filterPanel.contains(e.target) && !buttonFilter.contains(e.target)) {
+		isPanelVisibility("closed");
+	}
+});
